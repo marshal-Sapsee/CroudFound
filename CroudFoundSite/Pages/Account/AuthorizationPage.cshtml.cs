bc025e9c3e7cd4462d7568ac06bfc3ed8fe97c15
Sapsee <mail.sapsee@gmail.com>
2023-04-18 17:21:20 +0300

    Создание двух новых проектов

---
+++ CroudFoundSite/Pages/Account/AuthorizationPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CroudFoundSite.Pages.Account
+{
+    public class AuthorizationPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ CroudFoundSite/Pages/Account/RegistrationPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CroudFoundSite.Pages.Account
+{
+    public class RegistrationPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
